eb886e47a2d0babebdac8b1ecd9b3f041c3a408f
Madmvniuz <max.hofmann.v@gmail.com>
2022-05-01 21:54:16 -0400

    Update Proyecto 1.R

--- Proyecto 1.R
+++ Proyecto 1.R
@@ -1,40 +1,28 @@
-#Importamos librerias
-pacman::p_load(tidyverse, Rtsne, ggdendro ,magick ,  mclust, spotifyr, knitr, ggjoy)
-
-#conectamos a la api de spotify
-
-#probando si funciona conectarse directamente a la API
-
+pacman::p_load(tidyverse, Rtsne, ggdendro ,magick ,purrr ,mclust, spotifyr, knitr, ggjoy,data.table, fpc, dplyr, dendextend )#Importamos librerias
+#Accedemos a spotify
 Sys.setenv(SPOTIFY_CLIENT_ID = '0401714112b14f72ae4e0fc355244ece')
 Sys.setenv(SPOTIFY_CLIENT_SECRET = '159cf2594c864acdb0424b819263cda5')
-acces_token <- get_spotify_access_token()
+access_token <- get_spotify_access_token()
+auth_token <- get_spotify_authorization_code(scope = scope)
+#user_id <- readline(prompt = "Ingrese su user id: ")
+#nombre_play = readline(prompt="Ingrese nombre la playlist a crear: ")
+Cancion_inicial = readline(prompt="Ingrese la cancion para buscar: ") #pedimos la cancion para buscarla en la API
+Busqueda = search_spotify(Cancion_inicial, type = c("track"),market = NULL, limit = 20, offset = 0, include_external = NULL,authorization = get_spotify_access_token(),include_meta_info = FALSE) #busqueda de spotifyr
+Filtrada = subset(Busqueda, select= c("name", "album.name", "uri", "id"))#busqueda filtrada
+print(Filtrada)
+selected_search <- readline(prompt = "Escoja la cancion utilizando su numero asociado:  ") #escojemos la cancion de la lista 
+Cancion_inicial = Filtrada[selected_search,3:4] #filtramos para que solo queden uri e id
+id_cancion = pull(Cancion_inicial, var=2) #selecciona id
+uri_cancion= pull(Cancion_inicial, var=1) #selecciona uri
 
-brujeria <- get_artist_audio_features('brujeria')
-
-brujeria %>%
-  arrange(valence) %>%
-  select(track_name,valence)%>%
-  head(5)%>%
-  kable()
-
-ggplot(brujeria, aes(x= valence, y= album_name))+
-  geom_joy()
-  theme_joy()
-  
-
-
-
-
+#Tenemos nuestra cancion lista (uri e id)
 
 #-------------------------------------------------------------------------------------------------------
-#setiamos la semilla Negativa
-set.seed(-1)
 
-#Cargamos la base de datos de Prueba
-beats <- readRDS("C:/Users/maxho/OneDrive - Universidad Adolfo Ibanez/GITHUB/Mineria_De_Datos/Data/beats.rds")
+beats <- readRDS("C:/Users/maxho/OneDrive - Universidad Adolfo Ibanez/GITHUB/Mineria_De_Datos/Data/beats.rds")#Cargamos la base de datos de Prueba
 
 #Limpiamos las columnas que nos serviran para la clusterizacion 
-
+#---------------------------------------------------
 beats$album_type <- NULL
 beats$album_release_date <- NULL
 beats$album_release_year <- NULL
@@ -59,13 +47,24 @@ beats$artist_id <- NULL
 beats$artist_name<- NULL
 beats$track_preview_url<- NULL
 beats$disc_number<- NULL
-
-#pasamos de milisegundos a minutos para un trabajo mas agradable XD
-beats$duration_min <- beats$duration_ms/60000
-
-#borramos la columna de milisegundos
+beats$track_id<- NULL
 beats$duration_ms<- NULL
 
+#-------------------------------------------
 
-#Una vez listos los datos aplicamos el modelo 1 (dejamso track id y track Uri para ingresarlas a la futura playlist)
-
+beats <- unique(beats,by="track_uri") #hacemos unicos cada dato para que no hayan redundancias
+features = get_track_audio_features(id_cancion, authorization = get_spotify_access_token()) #con esto extraemos los datos importantes de la cancion directamente desde la api
+#filtramos los datos para ingresarlos a beats
+features_filtrado = features
+features_filtrado$type <- NULL
+features_filtrado$id <- NULL
+features_filtrado$key <- NULL
+features_filtrado$duration_ms <- NULL
+features_filtrado$analysis_url <- NULL
+features_filtrado$mode <- NULL
+features_filtrado$track_href <- NULL
+features_filtrado$time_signature <- NULL
+names(features_filtrado)[12]<- 'track_uri'
+#--------------------------------------------
+beats_con_cancion <-rbind(beats,features_filtrado) #aca anadimos la cancion de la API a la base de datos
+rownames(beats_con_cancion)<- beats_con_cancion$track_uri #ponemos los uri como el identificador 
